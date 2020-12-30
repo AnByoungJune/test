45eb2705979f6a381e7e1c42a1582c6fd6abd49d
bjan <bjan@sphinfo.co.kr>
2020-12-31 00:05:53 +0900

    test

--- TEST/src/main/java/com/jpa/demo/TestApplication.java
+++ TEST/src/main/java/com/jpa/demo/TestApplication.java
@@ -2,7 +2,13 @@ package com.jpa.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping(value = "/")
 @SpringBootApplication
 public class TestApplication {
 
@@ -10,4 +16,8 @@ public class TestApplication {
 		SpringApplication.run(TestApplication.class, args);
 	}
 
+	@GetMapping("/test")
+    public String getUser(Model model) {
+        return "test";
+    }
 }
